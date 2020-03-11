3147eb6812c929bdc54ba8e35e88e90797897277
Maks <m.rogach777@gmail.com>
2020-03-11 10:18:37 +0200

    first commit

---
+++ Bon_Voyage/Controllers/AdminControllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bon_Voyage.Controllers.AdminControllers
+{
+    [Authorize(Roles = "Admin")]
+    [Produces("application/json")]
+    [Route("api/admin/[controller]")]
+    public class AdminController : ControllerBase
+    {
+
+    }
+}
