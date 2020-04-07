ff82325a911fe5408f322f043c7fd60a8edef002
Maks <m.rogach777@gmail.com>
2020-04-07 18:30:12 +0300

    add Change Controller

--- Bon_Voyage/Controllers/ChangeControllers/ChangeController.cs
+++ Bon_Voyage/Controllers/ChangeControllers/ChangeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bon_Voyage.MediatR.User.Command.ChangeImageCommand;
+using Bon_Voyage.MediatR.User.Queries.GetImageQuery;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,5 +11,21 @@ namespace Bon_Voyage.Controllers.ChangeControllers
 {
     public class ChangeController : ApiController
     {
+        [HttpGet("get-image")]
+        public async Task<IActionResult> GetImage()
+        {
+            var id = User.Claims.ToList()[0].Value;
+            var res = await Mediator.Send(new GetImageQuery { Id = id });
+            return Ok(res);
+        }
+
+        [HttpPost("change-image")]
+        [RequestSizeLimit(100 * 1024 * 1024)]
+        public async Task<IActionResult> ChangeImage([FromBody] string photo)
+        {
+            var id = User.Claims.ToList()[0].Value;
+            var res = await Mediator.Send(new ChangeImageCommand { Photo = photo, Id = id });
+            return Ok(res);
+        }
     }
 }
