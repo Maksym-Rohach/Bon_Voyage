845f50ce385241ca42cc457896dd8633502ad4b8
Chadoboy <nazar-90@ukr.net>
2020-04-07 20:58:24 +0300

    Add ForgotPassword Backend

---
+++ Bon_Voyage/MediatR/Account/Commands/ForgotPassword/ForgotPasswordCommand.cs
@@ -0,0 +1,46 @@
+﻿using Bon_Voyage.DB;
+using Bon_Voyage.MediatR.Helpers;
+using Bon_Voyage.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bon_Voyage.MediatR.Account.Commands.ForgotPassword
+{
+    public class ForgotPasswordCommand : IRequest<ForgotPasswordViewModel>
+    {
+        public string Email { get; set; }
+
+        public class ForgotPasswordCommandHandler : BaseMediator, IRequestHandler<ForgotPasswordCommand, ForgotPasswordViewModel>
+        {
+            public ForgotPasswordCommandHandler(EFDbContext context) : base(context)
+            {
+
+            }
+            public async Task<ForgotPasswordViewModel> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+            {
+                var user = _context.Users.FirstOrDefault(u => u.Email == request.Email);
+                if (user == null)
+                {
+                    return new ForgotPasswordViewModel { Status = false, ErrorMessage = "Цей Емейл вже існує" };
+                }
+
+                var userName = user.Email;
+
+                EmailService emailService = new EmailService();
+                string url = "http://localhost:57206/Account/ChangePassword/" + user.Id;
+
+                await emailService.SendEmailAsync(request.Email, "ForgotPassword",
+                    $" Dear {userName}," +
+                    $" <br/>" +
+                    $" To change your password" +
+                    $" <br/>" +
+                    $" Зміна паролю <a href='{url}'>press</a>");
+                return new ForgotPasswordViewModel { Status = true };
+            }
+        }
+    }
+}
---
+++ Bon_Voyage/MediatR/Account/Commands/ForgotPassword/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bon_Voyage.MediatR.Account.Commands.ForgotPassword
+{
+    public class ForgotPasswordViewModel
+    {
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
