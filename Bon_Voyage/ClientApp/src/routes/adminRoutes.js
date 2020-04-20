22567cc1c3e28904350d96fa2553af97677d0a17
Bandera <42476974+Bandera1@users.noreply.github.com>
2020-04-20 21:55:45 +0300

    Add CountryControl folder with files Add routes

--- Bon_Voyage/ClientApp/src/routes/adminRoutes.js
+++ Bon_Voyage/ClientApp/src/routes/adminRoutes.js
@@ -3,12 +3,14 @@ import AirportControl from '../views/adminViews/AirportControl';
 
 const ManagerControl = React.lazy(() => import("../views/adminViews/ManagerControl"));
 const Login = React.lazy(() => import("../views/othersViews/LoginPage"));
+const CountryControl = React.lazy(() => import('../views/adminViews/CountryControl/CountryControlView'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/admin', exact: true, name: 'ManagerControl', component: ManagerControl },
   { path: '/admin/login', name: 'Login', component: Login },
   { path: '/admin/airport/control', name: 'AirportControl', component: AirportControl },
+  { path: '/admin/country/control', name: 'CountryControl', component: CountryControl},
 ];
 
 export default routes;
---
+++ Bon_Voyage/ClientApp/src/views/adminViews/CountryControl/CountryControlView.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class CountryControl extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+                PAGE
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default CountryControl;
