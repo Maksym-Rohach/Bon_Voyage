b7191469c9f7c2d0bcfdcc68affa1ff5e18ca85b
Maks <m.rogach777@gmail.com>
2020-04-29 18:50:22 +0300

    fix navbar for Contacts

--- Bon_Voyage/ClientApp/src/views/othersViews/ContactPage/ContactPage.jsx
+++ Bon_Voyage/ClientApp/src/views/othersViews/ContactPage/ContactPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../instruments/css/style.scss';
 import '../instruments/vendors/linericon/style.css'
 import mylogo from '../../../assets/img/Logo.png';
@@ -9,7 +10,7 @@ class Contact extends Component {
     return(
       
 <div className="home-page bg-white">
-<header className="header_area">
+{/* <header className="header_area">
     <div className="header-top">
       <div className="container">
         <div className="d-flex align-items-center">
@@ -35,9 +36,9 @@ class Contact extends Component {
         </div>
       </div>
     </div>
-    </header>  
+    </header>   */}
 
-    <div className="main_menu">
+    {/* <div className="main_menu">
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container">
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -74,7 +75,50 @@ class Contact extends Component {
           </ul>
         </div>
       </nav>
+    </div> */}
+
+
+<header className="header_area">
+
+<div className="main_menu">
+  <nav className="navbar navbar-expand-lg navbar-light">
+    <div className="container">
+      <a className="navbar-brand logo_h" href="index.html"><img src={mylogo} height="50px" alt="" /></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <div className="collapse navbar-collapse offset" id="navbarSupportedContent">
+        <ul className="nav navbar-nav menu_nav">
+          <li className="nav-item active"><a className="nav-link" href="index.html">Home</a></li>
+          <li className="nav-item"> <Link className="nav-link" to="/contact-page">Про нас</Link></li>
+          <li className="nav-item"><a className="nav-link" href="properties.html">Properties</a></li>
+          <li className="nav-item"><a className="nav-link" href="gallery.html">Gallery</a></li>
+        </ul>       
+      </div>
+      <div className="ml-auto d-none d-md-block d-md-flex" >
+            <div className="media header-top-info">
+              <span className="header-top-info__icon"><i className="fa fa-phone"></i></span>
+              <div className="media-body">
+                <p>Have any question?</p>
+                <p>Free: <a href="tel:+12 365 5233">+12 365 5233</a></p>
+              </div>
+            </div>
+            <div className="media header-top-info">
+              <span className="header-top-info__icon"><i className="fa fa-email"></i></span>
+              <div className="media-body">
+                <p>Have any question?</p>
+                <p>Free: <a href="tel:+12 365 5233">+12 365 5233</a></p>
+              </div>
+            </div>
+          </div>
     </div>
+  </nav>
+</div>
+</header>
+
   
   <section className="contact-banner-area" id="contact">
     <div className="container h-100">
