76c6e8fa6c41bb1b1c7e7a003a330d884bc3c197
Bandera <42476974+Bandera1@users.noreply.github.com>
2020-05-02 19:07:04 +0300

    Add footer Change faveicon

--- Bon_Voyage/ClientApp/src/views/othersViews/HomePage/HomePage.js
+++ Bon_Voyage/ClientApp/src/views/othersViews/HomePage/HomePage.js
@@ -327,6 +327,64 @@ class HomePage extends Component {
 
           
           
+
+            <footer class="footer-area section-gap">
+              {/* <div className="container">
+                <div class="row">
+                  <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Наші продукти</h4>
+                    <ul>
+                      <li><a href="#">Калькулятор на днопаскалі</a></li>
+                      <li><a href="#">Менджер паролів на плюсах</a></li>
+                      <li><a href="#">Недороблена курсова на MVC</a></li>
+                      <li><a href="#">Marketing Service</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Quick Links</h4>
+                    <ul>
+                      <li><a href="#">Jobs</a></li>
+                      <li><a href="#">Brand Assets</a></li>
+                      <li><a href="#">Investor Relations</a></li>
+                      <li><a href="#">Terms of Service</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Features</h4>
+                    <ul>
+                      <li><a href="#">Jobs</a></li>
+                      <li><a href="#">Brand Assets</a></li>
+                      <li><a href="#">Investor Relations</a></li>
+                      <li><a href="#">Terms of Service</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Resources</h4>
+                    <ul>
+                      <li><a href="#">Guides</a></li>
+                      <li><a href="#">Research</a></li>
+                      <li><a href="#">Experts</a></li>
+                      <li><a href="#">Agencies</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-xl-4 col-md-8 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Newsletter</h4>
+                    <p>You can trust us. we only send promo offers,</p>
+                    <div class="form-wrap" id="mc_embed_signup">
+                      <form target="_blank"
+                        method="get" class="form-inline">
+                        <input class="form-control" name="EMAIL" placeholder="Your Email Address"
+                          required="" type="email" />
+                        <button class="click-btn btn btn-default text-uppercase">subscribe</button>
+                        <div class="info"></div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
+            </footer>
+
+
           </main>
         </div>
       </React.Fragment>
