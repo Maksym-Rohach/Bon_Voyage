bed8b92b2dbac237b46e2c91827f2575edf62735
Chadoboy <nazar-90@ukr.net>
2020-05-05 18:52:47 +0300

    add gallery page

--- Bon_Voyage/Controllers/ClientControllers/HomePageController.cs
+++ Bon_Voyage/Controllers/ClientControllers/HomePageController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Bon_Voyage.MediatR.Cities.Queries.GetCitiesByCountry;
 using Bon_Voyage.MediatR.Home.Queries;
+using Bon_Voyage.MediatR.Hotel.Queries.GetAllHotelsPhotos;
 using Bon_Voyage.MediatR.Hotel.Queries.GetHotelsByCity;
 using Bon_Voyage.MediatR.Manager.Queries.GetAllManagersQuery;
 using Microsoft.AspNetCore.Authorization;
@@ -19,6 +20,18 @@ namespace Bon_Voyage.Controllers.ClientControllers
         public async Task<IActionResult> GetHomeInformation()
         {
             var result = await Mediator.Send(new GetHomeInformationQuery());
+            if (result == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetGalleryPhoto")]
+        public async Task<IActionResult> GetGalleryPhoto()
+        {
+            var result = await Mediator.Send(new GetAllHotelsPhotosQuery());
             if (result == null)
             {
                 return BadRequest();
---
+++ Bon_Voyage/MediatR/Hotel/ViewModels/AllHotelPhotosViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bon_Voyage.MediatR.Hotel.ViewModels
+{
+    public class AllHotelPhotosViewModel
+    {
+        public string Id { get; set; }
+        public string Photo { get; set; }
+    }
+}
