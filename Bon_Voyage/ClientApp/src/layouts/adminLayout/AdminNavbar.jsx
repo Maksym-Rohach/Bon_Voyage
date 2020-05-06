8909a31ddb25ea69dd7a8b3407ad7b6f5f0be49e
Astaniel <novdmiol@gmail.com>
2020-05-07 01:29:59 +0300

    Fixed all bags in navbars

--- Bon_Voyage/ClientApp/src/layouts/adminLayout/AdminNavbar.jsx
+++ Bon_Voyage/ClientApp/src/layouts/adminLayout/AdminNavbar.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import mylogo from '../../assets/img/Logo.png';
 import { AppAsideToggler, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 
-
 const propTypes = {
   children: PropTypes.node,
 };
@@ -14,68 +13,26 @@ const defaultProps = {};
 
 class AdminNavbar extends Component {
   render() {
-
-    // eslint-disable-next-line
     const { children, image, ...attributes } = this.props;
-
     return (
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />        
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-        <Link className="navbar-brand logo_h" to="/"><img src={mylogo} height="50px" alt="" /></Link>
-        <Nav className="d-md-down-none" navbar>
-          {/* <NavItem className="px-3">
-            <NavLink to="/dashboard" className="nav-link" >Dashboard</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <Link to="/users" className="nav-link">Users</Link>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink to="#" className="nav-link">Settings</NavLink>
-          </NavItem> */}
-        </Nav>
-        <Nav className="ml-auto" navbar>
-          {/* <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link"><i className="icon-list"></i></NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link"><i className="icon-location-pin"></i></NavLink>
-          </NavItem>
-           */}
+        <AppSidebarToggler className="d-md-down-none" display="lg" />    
+        <Link className="ml-4 navbar-brand logo_h"  to="/"><img src={mylogo} height="50px"  alt="" /></Link>
+        <Nav className="ml-auto" navbar>       
           <UncontrolledDropdown nav direction="down">
             <DropdownToggle nav>
               <img src={image} className="img-avatar" alt="" />
             </DropdownToggle>
             <DropdownMenu right>
-              {/* <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
-              <DropdownItem><i className="fa fa-bell-o"></i> Updates<Badge color="info">42</Badge></DropdownItem>
-              <DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge color="success">42</Badge></DropdownItem>
-              <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
-              <DropdownItem><i className="fa fa-comments"></i> Comments<Badge color="warning">42</Badge></DropdownItem>
-              <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
-              <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
-              <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
-              <DropdownItem><i className="fa fa-usd"></i> Payments<Badge color="secondary">42</Badge></DropdownItem>
-              <DropdownItem><i className="fa fa-file"></i> Projects<Badge color="primary">42</Badge></DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem><i className="fa fa-shield"></i> Lock Account</DropdownItem> */}
               <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i>Вихід</DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
-        </Nav>
-        {/* <AppAsideToggler className="d-md-down-none" /> */}
-        {/* <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
-        /> */}
+        </Nav>       
       </React.Fragment>
     );
   }
 }
-
 AdminNavbar.propTypes = propTypes;
 AdminNavbar.defaultProps = defaultProps;
 
