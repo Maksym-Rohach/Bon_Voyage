9cce0f07c41fb0e478a2566f813bb1e44d811871
Andriy23780 <Andriy23780@gmail.com>
2020-05-06 17:23:02 +0300

    Get change

---
+++ Bon_Voyage/MediatR/User/Command/FeedbackCommand/FeedbackCommand.cs
@@ -0,0 +1,43 @@
+﻿using Bon_Voyage.DB;
+using Bon_Voyage.DB.IdentityModels;
+using Bon_Voyage.MediatR.Helpers;
+using Bon_Voyage.Services;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Bon_Voyage.DB;
+using Bon_Voyage.DB.IdentityModels;
+
+namespace Bon_Voyage.MediatR.User.Command.FeedbackCommand
+{
+    public class FeedbackCommand : IRequest<FeedbackViewModel>
+    {
+        public string Id { get; set; }
+        public string Theme { get; set; }
+        public string Message { get; set; }
+        public DateTime Data { get; set; }
+
+        public class FeedbackCommandHandler : BaseMediator, IRequestHandler<FeedbackCommand, FeedbackViewModel>
+        {
+            private readonly IJwtTokenService _IJwtTokenService;
+            private readonly UserManager<DbUser> _userManager;
+
+            public FeedbackCommandHandler(EFDbContext context, IJwtTokenService jwtToken, UserManager<DbUser> userManager) : base(context)
+            {
+                _IJwtTokenService = jwtToken;
+                _userManager = userManager;
+            }
+            Task<FeedbackViewModel> IRequestHandler<FeedbackCommand, FeedbackViewModel>.Handle(FeedbackCommand request, CancellationToken cancellationToken)
+            {
+                return null;
+            }
+        }
+    }
+}
+
+
